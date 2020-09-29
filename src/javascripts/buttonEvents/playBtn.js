48d15b73f9ec59f1bc01b442d838aa7a8f693998
Wanda <wmmcinturff@gmail.com>
2020-09-29 08:45:45 -0500

    divided button events into separate files

---
+++ src/javascripts/buttonEvents/playBtn.js
@@ -0,0 +1,29 @@
+let fun = 50;
+
+const superFun = () => {
+  if (fun >= 0 && fun < 100) {
+    fun += 50;
+  }
+  return fun;
+};
+
+const sortaFun = () => {
+  if (fun >= 0 && fun <= 100) {
+    fun += 2;
+  } else if (fun >= 0) {
+    fun = 100;
+  }
+  return fun;
+};
+
+const playButton = () => {
+  $('#btn1-super-fun-time').click(() => {
+    $('#play-score').html('');
+    $('#play-score').html(`<div>${superFun()}</div>`);
+  });
+  $('#btn2-sorta-fun').click(() => {
+    $('#play-score').html('');
+    $('#play-score').html(`<div>${sortaFun()}</div>`);
+  });
+};
+export default { playButton };
---
+++ src/javascripts/data/petData.js
@@ -0,0 +1,33 @@
+const petData = [
+  {
+    id: '1',
+    activity: 'eat',
+    score: 100,
+    button1: 'healthy',
+    button2: 'junk-food'
+  },
+  {
+    id: '2',
+    activity: 'play',
+    score: 50,
+    button1: 'super-fun-time',
+    button2: 'sorta-fun'
+  },
+  {
+    id: '3',
+    activity: 'fight',
+    score: 100,
+    button1: 'bravely-bugger-off',
+    button2: 'mortal-combat'
+  },
+  {
+    id: '4',
+    activity: 'sleep',
+    score: 100,
+    button1: 'nap',
+    button2: 'slumber'
+  },
+
+];
+
+export default { petData };
